6e1f5c1fd97b2dd3096ed208bdb21a00198938ab
Frank Hinek <frank.hinek@gmail.com>
2017-02-25 18:03:24 -0500

    Add TensorFlow model save and restore example

---
+++ 18-deep-neural-networks/tf-restore-model.py
@@ -0,0 +1,56 @@
+'''
+A simple example showing how to restore a trained TensorFlow model and its
+weights from disk.  Given the default hyperparameters, the restored model will
+have already been trained for 100 epochs.
+
+The accompanying tf-save-model.py script must be run first.
+'''
+
+import tensorflow as tf
+from tensorflow.examples.tutorials.mnist import input_data
+import numpy as np
+import math
+
+# The file path to save the data
+save_file = './train_model.ckpt'
+
+# Parameters
+learning_rate = 0.001
+n_input = 784  # MNIST data input (img shape: 28*28)
+n_classes = 10  # MNIST total classes (0-9 digits)
+
+# Import MNIST data
+mnist = input_data.read_data_sets('.', one_hot=True)
+
+# Features and Labels
+features = tf.placeholder(tf.float32, [None, n_input])
+labels = tf.placeholder(tf.float32, [None, n_classes])
+
+# Weights & bias
+weights = tf.Variable(tf.random_normal([n_input, n_classes]))
+bias = tf.Variable(tf.random_normal([n_classes]))
+
+# Logits - xW + b
+logits = tf.add(tf.matmul(features, weights), bias)
+
+# Define loss and optimizer
+cost = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(logits, labels))
+optimizer = tf.train.GradientDescentOptimizer(learning_rate=learning_rate)\
+    .minimize(cost)
+
+# Calculate accuracy
+correct_prediction = tf.equal(tf.argmax(logits, 1), tf.argmax(labels, 1))
+accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
+
+# Class used to save and/or restore Tensor model and variables
+saver = tf.train.Saver()
+
+# Launch the graph
+with tf.Session() as session:
+    saver.restore(session, save_file)
+
+    test_accuracy = session.run(
+        accuracy,
+        feed_dict={features: mnist.test.images, labels: mnist.test.labels})
+
+print('Test Accuracy: {}'.format(test_accuracy))
---
+++ 18-deep-neural-networks/tf-save-model.py
@@ -0,0 +1,92 @@
+'''
+A simple example showing how to save a trained TensorFlow model and its weights
+to disk.  Given the default hyperparameters, the model will be trained for 100
+epochs before being saved to disk.
+
+The accompanying tf-restore-model.py script demonstrates how to restore
+the trained model and weights.
+
+The following files should be written to disk assuming you are running
+TensorFlow 0.12 or later:
+t10k-images-idx3-ubyte.gz
+t10k-labels-idx1-ubyte.gz
+train-images-idx3-ubyte.gz
+train-labels-idx1-ubyte.gz
+checkpoint
+train_model.ckpt.data-00000-of-00001
+train_model.ckpt.index
+train_model.ckpt.meta
+'''
+
+import tensorflow as tf
+from tensorflow.examples.tutorials.mnist import input_data
+import numpy as np
+import math
+
+# The file path to save the data
+save_file = './train_model.ckpt'
+
+# Parameters
+learning_rate = 0.001
+n_input = 784  # MNIST data input (img shape: 28*28)
+n_classes = 10  # MNIST total classes (0-9 digits)
+batch_size = 128
+n_epochs = 100
+
+# Import MNIST data
+mnist = input_data.read_data_sets('.', one_hot=True)
+
+# Features and Labels
+features = tf.placeholder(tf.float32, [None, n_input])
+labels = tf.placeholder(tf.float32, [None, n_classes])
+
+# Weights & bias
+weights = tf.Variable(tf.random_normal([n_input, n_classes]))
+bias = tf.Variable(tf.random_normal([n_classes]))
+
+# Logits - xW + b
+logits = tf.add(tf.matmul(features, weights), bias)
+
+# Define loss and optimizer
+cost = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(logits=logits, labels=labels))
+optimizer = tf.train.GradientDescentOptimizer(learning_rate=learning_rate).minimize(cost)
+
+# Calculate accuracy
+correct_prediction = tf.equal(tf.argmax(logits, 1), tf.argmax(labels, 1))
+accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
+
+# Class used to save and/or restore Tensor model and variables
+saver = tf.train.Saver()
+
+# Initializing the variables
+init = tf.global_variables_initializer()
+
+# Launch the graph
+with tf.Session() as session:
+    session.run(init)
+
+    # Training cycle
+    for epoch in range(n_epochs):
+        total_batch = math.ceil(mnist.train.num_examples / batch_size)
+
+        # Loop over all batches
+        for i in range(total_batch):
+            batch_features, batch_labels = mnist.train.next_batch(batch_size)
+            session.run(
+                optimizer,
+                feed_dict={features: batch_features, labels: batch_labels})
+
+        # Print status for every 10 epochs
+        if epoch % 10 == 0:
+            valid_accuracy = session.run(
+                accuracy,
+                feed_dict={
+                    features: mnist.validation.images,
+                    labels: mnist.validation.labels})
+            print('Epoch {:<3} - Validation Accuracy: {}'.format(
+                epoch,
+                valid_accuracy))
+
+    # Save the model
+    saver.save(session, save_file)
+    print('Trained Model Saved.')
